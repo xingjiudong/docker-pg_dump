f1647a6086ab80988232b9ffe26f03f5db6e254f
xingjiudong <25635680@qq.com>
2017-12-11 20:57:39 +0800

    delete test files

--- confd/confd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ./env.config
-
-#sed -r "s/@PROJECT_NAME@/${PROJECT_NAME}/g" /etc/confd/templates/haproxy.cfg.tmpl.in > /etc/confd/templates/haproxy.cfg.tmpl 
-
-confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP}:2379 --prefix="${PROJECT_NAME}"
